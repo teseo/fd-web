8816d6a54bd60233e251d886b709a055e9282ba0
teseo <sol@javiermellado.com>
2020-03-07 20:05:36 +0000

    extracted Game styled components to its own file

---
+++ src/Components/Game/StyledComponents/index.tsx
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+import {LOGO_URL} from "../../constants";
+import WinnerImageSrc from "../../../assets/winner.jpg";
+import TryAgainSrc from "../../../assets/tryagain.jpg";
+
+export const GameContainer = styled.div`
+  padding: 10px 30px;
+  margin: 0 auto;
+  max-width: 1441px;
+  background-color: #152a47;
+  flex-direction: column;
+`;
+export const GlobalHeader = styled.div`
+  padding-bottom: 19px;
+  text-align: center;
+`;
+export const TopLogoImage = styled.img.attrs({
+  src: LOGO_URL
+})`
+height: 28px;
+`;
+export const WinnerImage = styled.img.attrs({
+  src: WinnerImageSrc
+})`
+height: 192px;
+`;
+export const TryAgainImage = styled.img.attrs({
+  src: TryAgainSrc
+})`
+height: 192px;
+`;
+export const TopLogoLink = styled.a``;
+export const HeaderContainer = styled.div`
+  align-items: center;
+  background: radial-gradient(ellipse, #0facfd 16%, #1493ff 100%);
+  color: #ffff;
+  justify-content: center;  
+  margin: 0 5px 5px 0;
+  padding: 5px;
+  border-radius: 16px;
+`;
+export const HeaderText = styled.span`
+  margin: 0 5px 0 5px;
+  font-size: 18px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const BottomContainer = styled.div`
+  display: flex;
+  height: 25px;
+  background: radial-gradient(ellipse, #0facfd 16%, #1493ff 100%);
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px 5px 0;
+  border-radius: 16px;
+  padding: 5px;
+`;
+export const RestartButtonContainer = styled.div`
+display: flex;
+  flex-direction: column;
+  background-color: #1493ff;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px 5px 0;
+  padding: 5px;
+  border-radius: 16px;
+`;
+export const ContinueButtonContainer = styled.button`
+  background-color: #fff;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  border-radius: 12px;
+  padding: 5px;
+  margin-left: 5px;
+`;
+export const BottomText = styled.p`
+color: #ffff;
+`;
+export const ButtonText = styled.p`
+color: #1493ff;
+background: #fff;
+`;
+export const PlayersCardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  margin-bottom: 5px;
+  border-radius: 12px;
+`;
--- src/Components/Game/index.tsx
+++ src/Components/Game/index.tsx
@@ -1,99 +1,17 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import CounterPanel from "../CounterPanel";
-import { PENDING_GUESS, LOGO_URL, SUCCESSFUL_GUESS, FAILED_GUESS, MAX_SCORE } from "../constants";
+import { PENDING_GUESS, SUCCESSFUL_GUESS, FAILED_GUESS, MAX_SCORE } from "../constants";
 import ApiService from "../utils";
 import { Player, StateType } from "../interfaces";
 import PlayerCard from "../PlayerCard";
-import WinnerImageSrc from "../../assets/winner.jpg";
-import TryAgainSrc from "../../assets/tryagain.jpg";
-const GameContainer = styled.div`
-  padding: 10px 30px;
-  margin: 0 auto;
-  max-width: 1441px;
-  background-color: #152a47;
-  flex-direction: column;
-`;
-const GlobalHeader = styled.div`
-  padding-bottom: 19px;
-  text-align: center;
-`;
-const TopLogoImage = styled.img.attrs({
-  src: LOGO_URL
-})`
-height: 28px;
-`;
-const WinnerImage = styled.img.attrs({
-  src: WinnerImageSrc
-})`
-height: 192px;
-`;
-const TryAgainImage = styled.img.attrs({
-  src: TryAgainSrc
-})`
-height: 192px;
-`;
-const TopLogoLink = styled.a``;
-const HeaderContainer = styled.div`
-  align-items: center;
-  background: radial-gradient(ellipse, #0facfd 16%, #1493ff 100%);
-  color: #ffff;
-  justify-content: center;  
-  margin: 0 5px 5px 0;
-  padding: 5px;
-  border-radius: 16px;
-`;
-const HeaderText = styled.span`
-  margin: 0 5px 0 5px;
-  font-size: 18px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const BottomContainer = styled.div`
-  display: flex;
-  height: 25px;
-  background: radial-gradient(ellipse, #0facfd 16%, #1493ff 100%);
-  justify-content: center;
-  align-items: center;
-  margin: 0 5px 5px 0;
-  border-radius: 16px;
-  padding: 5px;
-`;
-const RestartButtonContainer = styled.div`
-display: flex;
-  flex-direction: column;
-  background-color: #1493ff;
-  justify-content: center;
-  align-items: center;
-  margin: 0 5px 5px 0;
-  padding: 5px;
-  border-radius: 16px;
-`;
-const ContinueButtonContainer = styled.button`
-  background-color: #fff;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  border-radius: 12px;
-  padding: 5px;
-  margin-left: 5px;
-`;
-const BottomText = styled.p`
-color: #ffff;
-`;
-const ButtonText = styled.p`
-color: #1493ff;
-background: #fff;
-`;
-const PlayersCardContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
-  margin-bottom: 5px;
-  border-radius: 12px;
-`;
+import {
+  BottomContainer, BottomText,
+  ButtonText, ContinueButtonContainer, GameContainer,
+  GlobalHeader, HeaderContainer,
+  HeaderText,
+  PlayersCardContainer, RestartButtonContainer, TopLogoImage, TopLogoLink, TryAgainImage,
+  WinnerImage
+} from "./StyledComponents";
 
 const Game: React.FC = () =>  {
   const initialState = {
