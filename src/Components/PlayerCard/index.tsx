68fde7022af57cffa30750225cbcb95b26a80d5b
Javier Mellado <sol@javiermellado.com>
2020-03-09 09:58:46 +0000

    rolling back changes

--- src/Components/PlayerCard/index.tsx
+++ src/Components/PlayerCard/index.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { PlayerProps } from "../interfaces";
-import { PlayerDataContainer, PlayerImage, TextName } from "./StyledComponents";
+import { PlayerCardContainer, PlayerDataContainer, PlayerImage, TextName } from "./StyledComponents";
 
 const PlayerCard: React.FC<PlayerProps> = props => {
   const { firstName, lastName, ffpg, showResult, handlePlayerClick, imageSource } = props;
   const name = firstName + " " + lastName;
   return (
-    <PlayerDataContainer onClick={handlePlayerClick}>
-      <PlayerImage src={imageSource.uri} />
-      <TextName>Name: {name}</TextName>
-      {showResult && ffpg && <TextName>FFPG: {ffpg.toPrecision(10)}</TextName>}
-    </PlayerDataContainer>
+    <PlayerCardContainer>
+      <PlayerDataContainer onClick={handlePlayerClick}>
+        <PlayerImage src={imageSource.uri} />
+        <TextName>Name: {name}</TextName>
+        {showResult && ffpg && <TextName>FFPG: {ffpg.toPrecision(10)}</TextName>}
+      </PlayerDataContainer>
+    </PlayerCardContainer>
   );
 };
 export default PlayerCard;
